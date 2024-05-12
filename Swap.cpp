8e145df628f8042f62818d024cc745c7ac1c93cf
pplsk2524 <144938245+pplsk2524@users.noreply.github.com>
2024-05-12 18:22:15 +0530

    Create Swap.cpp

---
+++ Swap.cpp
@@ -0,0 +1,12 @@
+//User function Template for C++
+
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+        //complete the function here
+        pair<int,int> n;
+        n.first=b;
+        n.second=a;
+        return n;
+    }
+};
